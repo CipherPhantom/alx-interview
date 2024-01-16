df86e8d87a7dc58cb6e4e3dd617cdc41c1e2213a
CipherPhantom <oyepitan10@gmail.com>
2024-01-16 08:31:09 +0100

    0x02-minimum_operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+In a text file, there is a single character H. Your text editor
+can execute only two operations in this file: Copy All and Paste.
+Given a number n, write a method that calculates the
+fewest number of operations needed to result in exactly n H characters
+in the file.
+"""
+
+
+def copyAll(string):
+    """Returns input string"""
+    return string
+
+
+def paste(string_1, string_2):
+    """Concatenates two string"""
+    return string_1 + string_2
+
+
+def minOperations(n):
+    """Calculates and Returns the fewest number of operations"""
+    operations = 0
+    txt_file = "H"
+
+    if type(n) != int or n <= 1:
+        return operations
+
+    while len(txt_file) < n:
+        if n % len(txt_file) == 0:
+            copy = copyAll(txt_file)
+            txt_file = paste(txt_file, copy)
+            operations += 2
+        else:
+            txt_file = paste(txt_file, copy)
+            operations += 1
+    return operations
